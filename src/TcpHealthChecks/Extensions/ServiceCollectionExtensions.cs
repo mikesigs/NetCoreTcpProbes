2415c5e170fdb103d1339ada2c8fa50bb2cc957f
Mike <mikesigsworth@gmail.com>
2019-03-10 13:06:13 -0600

    Split up extension methods so user can enable one or both checks.

--- src/TcpHealthChecks/Extensions/ServiceCollectionExtensions.cs
+++ src/TcpHealthChecks/Extensions/ServiceCollectionExtensions.cs
@@ -1,4 +1,4 @@
-using System.Net;
+﻿using System.Net;
 using Microsoft.Extensions.DependencyInjection;
 using TcpHealthChecks.Core;
 using TcpHealthChecks.HealthChecks;
@@ -8,15 +8,33 @@ namespace TcpHealthChecks.Extensions
 {
     public static class ServiceCollectionExtensions
     {
+        /// <summary>
+        /// Enables both Readiness and Liveness TCP Listeners with default Health Checks
+        /// </summary>
+        /// <param name="services"></param>
         public static void AddTcpHealthChecks(this IServiceCollection services)
         {
             AddLivenessTcpHealthCheck(services);
             AddReadinessTcpHealthCheck(services);
         }
 
+        /// <summary>
+        /// Enables Liveness TCP Listener with default Liveness Health Check
+        /// </summary>
+        /// <param name="services"></param>
+        public static void AddLivenessTcpHealthCheck(IServiceCollection services)
+        {
             services.AddSingleton<IHealthCheck, LivenessHealthCheck>();
-            services.AddSingleton<IHealthCheck, ReadinessHealthCheck>();
             services.AddHostedService<LivenessTcpListener>();
+        }
+
+        /// <summary>
+        /// Enables Readiness TCP Listener with default Readiness Health Check
+        /// </summary>
+        /// <param name="services"></param>
+        public static void AddReadinessTcpHealthCheck(IServiceCollection services)
+        {
+            services.AddSingleton<IHealthCheck, ReadinessHealthCheck>();
             services.AddHostedService<ReadinessTcpListener>();
         }
     }
