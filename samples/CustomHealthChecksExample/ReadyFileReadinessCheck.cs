d0a47bdca09adff7d67d3eda3a7038def9d80be3
Mike <mikesigsworth@gmail.com>
2019-03-09 17:26:58 -0700

    Added delayed file creation to custom health checks.

--- samples/CustomHealthChecksExample/ReadyFileReadinessCheck.cs
+++ samples/CustomHealthChecksExample/ReadyFileReadinessCheck.cs
@@ -1,10 +1,20 @@
-﻿using System.IO;
+﻿using System;
+using System.IO;
+using System.Threading.Tasks;
 using TcpHealthChecks.Core;
 
 namespace CustomHealthChecksExample
 {
     public class ReadyFileReadinessCheck : IHealthCheck
     {
+        static ReadyFileReadinessCheck()
+        {
+            // Creates the 'ready' file 10 seconds after app start
+            var readyTask = Task
+                .Delay(TimeSpan.FromSeconds(10))
+                .ContinueWith(_ => File.Create("ready"));
+        }
+
         public HealthCheckKind Kind => HealthCheckKind.Readiness;
         public bool IsHealthy() => File.Exists("ready");
     }
--- samples/CustomHealthChecksExample/UnhealthyFileLivenessCheck.cs
+++ samples/CustomHealthChecksExample/UnhealthyFileLivenessCheck.cs
@@ -1,10 +1,19 @@
-﻿using System.IO;
+﻿using System;
+using System.IO;
+using System.Threading.Tasks;
 using TcpHealthChecks.Core;
 
 namespace CustomHealthChecksExample
 {
     public class UnhealthyFileLivenessCheck : IHealthCheck
     {
+        static UnhealthyFileLivenessCheck()
+        {
+            // Creates the 'unhealthy' file 30 seconds after app start
+            Task.Delay(TimeSpan.FromSeconds(30))
+                .ContinueWith(_ => File.Create("unhealthy"));
+
+        }
         public HealthCheckKind Kind => HealthCheckKind.Liveness;
         public bool IsHealthy() => !File.Exists("unhealthy");
     }
